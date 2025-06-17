b6627a03158e5c0d3d5f31499a1d9facf18d6668
Acelle Krislette Rosales <acellekrislette@gmail.com>
2025-06-17 18:52:57 +0800

    Corrected backprop imple

--- and-ann-optimized/core/backpropagation.py
+++ and-ann-optimized/core/backpropagation.py
@@ -1,13 +1,3 @@
-def perform_backpropagation(
-    w1_old: float,
-    w2_old: float,
-    b1_old: float,
-    de_dw1: float,
-    de_dw2: float,
-    de_db1: float,
-    learning_rate: float,
-) -> tuple[float, float, float]:
-    w1_new = w1_old - (learning_rate * de_dw1)
-    w2_new = w2_old - (learning_rate * de_dw2)
-    b1_new = b1_old - (learning_rate * de_db1)
-    return w1_new, w2_new, b1_new
+def perform_backpropagation(de_dout: float, dout_dnet: float, input: float) -> float:
+    # This follows the formula: (dE/dO) * (dO/dnet) * (dnet/d<input>) [chain rule]
+    return de_dout * dout_dnet * input
