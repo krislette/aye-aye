4e272e7e87db2c9e6f23a70e5193e6b1a1872139
Acelle Krislette Rosales <acellekrislette@gmail.com>
2025-06-19 12:58:01 +0800

    Adjusted invocation with params

--- xnor-ann/main.py
+++ xnor-ann/main.py
@@ -2,8 +2,9 @@ from core.xnor_ann import XnorAnn
 
 
 def main() -> None:
-    xnor_ann = XnorAnn()
-    xnor_ann.init_training_data()
+    # Params: Fan in, fan out, hidden neurons, LR
+    xnor_ann = XnorAnn(2, 1, 3, 0.01)
+    xnor_ann.init_weights()
 
 
 if __name__ == "__main__":
