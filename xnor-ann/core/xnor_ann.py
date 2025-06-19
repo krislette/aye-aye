Wrong and buggy feedforward & backprop D:

xnor-ann/core/xnor_ann.py
@@ -24,9 +24,9 @@ class XnorAnn:
         training_inputs = np.delete(training_data, 2, axis=1)
 
         # Colon (:) means all rows, and then (2) just column 2
-        training_outputs = training_data[:, 2]
+        training_targets = training_data[:, 2]
 
-        return training_inputs, training_outputs
+        return training_inputs, training_targets
 
     def init_weights(self) -> np.ndarray:
         # Initialize weights using He initialization (for ReLU)
@@ -50,21 +50,78 @@ class XnorAnn:
         # Then .astype(int) converts booleans to int -> [0, 1, 1, 0] <- ReLU derivative!!!
         return (values > 0).astype(int)
 
+    def calculate_mse(self, outputs: np.ndarray, targets: np.ndarray) -> float:
+        # Get the total number of training entries first
+        size = targets.shape[0]
+
+        # And then calculate MSE: summation of -> each (output - target)^2 / 2 then average
+        mse = np.sum(((outputs - targets) ** 2) / 2) / size
+        return mse
+
     def feedforward(
         self, inputs: np.ndarray, weights: np.ndarray, biases: np.ndarray
     ) -> tuple[np.ndarray, np.ndarray]:
         # Formula: (i1 * w1) + (i2 * w2) + bias
+        # Performs (1x2) dot (2x3) + (1x3) = (1x3)
         net_outputs = np.dot(inputs, weights) + biases
         outputs = self.relu(net_outputs)
+
+        # Returns the net output and activated output from 3 hidden neurons (1x3 matrix)
         return net_outputs, outputs
 
-    def backpropagation(self) -> None:
-        # 
-        pass
+    def backpropagation(
+        self,
+        net_outputs: np.ndarray,
+        outputs: np.ndarray,
+        inputs: np.ndarray,
+        targets: np.ndarray,
+    ) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+        # Get (dE/dO) and (dO/dnet)
+        de_dout = outputs - targets
+        dout_dnet = self.relu_derivative(net_outputs)
 
-    def train(self) -> None:
-        # TODO: Create main training logic
-        pass
+        # Then apply to the chain rule formula: (dE/dO) * (dO/dnet) * (dnet/d<input>)
+        de_dw = np.dot((de_dout * dout_dnet), inputs)
+        de_db = np.sum((de_dout * dout_dnet), axis=0, keepdims=True)
+
+        return de_dw, de_db
+
+    def update_parameters(
+        self, weight_gradients: np.ndarray, bias_gradients: np.ndarray
+    ) -> tuple[np.ndarray, np.ndarray]:
+        new_weights = weight_gradients - (self.learning_rate * weight_gradients)
+        new_biases = bias_gradients - (self.learning_rate * bias_gradients)
+        return new_weights, new_biases
+
+    def train(self) -> tuple[np.ndarray, np.ndarray]:
+        inputs, targets = self.init_training_data()
+        weights = self.init_weights()
+        biases = self.init_biases()
+
+        for epoch in range(self.target_epochs):
+            # Step 1: Feedforward
+            net_outputs, outputs = self.feedforward(inputs, weights, biases)
+
+            # Step 2: Calculate MSE
+            mean_squared_error = self.calculate_mse(outputs, targets)
+
+            # Step 3: Backpropagation
+            weight_gradients, bias_gradients = self.backpropagation(
+                net_outputs, outputs, inputs, targets
+            )
+
+            # Step 4: Update weights and biases
+            weights, biases = self.update_parameters(weight_gradients, bias_gradients)
+
+            # Check if network converged
+            if mean_squared_error <= self.target_error:
+                with open("data/optimized_parameters.txt", "w") as file:
+                    file.write(f"{weights},{biases}")
+                return weights, biases
+
+            # Print epoch every 100 for monitoring on the CLI
+            if epoch % 100 == 0:
+                print(f"Epoch: {epoch:<5} " f"| Error: {mean_squared_error:<12.8f} ")
 
     def predict(self) -> None:
         # TODO: Create prediction logic